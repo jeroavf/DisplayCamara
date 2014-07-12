c0f57902cbc2ee9cb88c8a688589263df3cfddf2
Jeronimo Avelar Filho <jeroavf@gmail.com>
2014-07-12 13:39:15 -0300

    Melhoria na inicialização da Ethernet
    Foi mudada a rotina de inicialização da ethernet. Esta inicialização ,
    em caso de erro , chamava a rotina setup de novo. Agora fica em loop
    até conseguir  inicializar a ethernet .

--- Arduino/ModuloDisplay/ModuloDisplay.ino
+++ Arduino/ModuloDisplay/ModuloDisplay.ino
@@ -1,22 +1,22 @@
 
 /*
   ModuloDisplay - 05/07/2014
-  Prototipo de display com exibicao de informacao por um servo-motor simulando um display analogico
-  e display lcd de 2 linhas por 16 caracteres
-  Arduino - Brasilia 
-  Autor : Jeronimo Avelar Filho
-  Este codigo e' baseado em exemplo existente em https://bitbucket.org/victoraldir/ajson-aviseja/src
-  
-  Observacoes: 
-  O shield ethernet utiliza os pinos do arduino abaixo:
-  D2 - Connects the INT pin of the W5100
-  D3 - Contral the reset of the W5100
-  D10 - Used for SPI Chip Select
-  D11 - Used for SPI MOSI
-  D12 - Used for SPI MISO
-  D13 - Used for SPI SCK
-  
-*/
+ Prototipo de display com exibicao de informacao por um servo-motor simulando um display analogico
+ e display lcd de 2 linhas por 16 caracteres
+ Arduino - Brasilia 
+ Autor : Jeronimo Avelar Filho
+ Este codigo e' baseado em exemplo existente em https://bitbucket.org/victoraldir/ajson-aviseja/src
+ 
+ Observacoes: 
+ O shield ethernet utiliza os pinos do arduino abaixo:
+ D2 - Connects the INT pin of the W5100
+ D3 - Contral the reset of the W5100
+ D10 - Used for SPI Chip Select
+ D11 - Used for SPI MOSI
+ D12 - Used for SPI MISO
+ D13 - Used for SPI SCK
+ 
+ */
 #include <SPI.h>
 #include <Ethernet.h>
 #include <aJSON.h>
@@ -54,42 +54,37 @@ void setup() {
   // Inicializa a Serial
   Serial.begin(9600);
   Serial.println("Iniciando ...") ;
-  
-   
+
+
   //Inicializa LCD
-   lcd.begin(16, 2);
-   lcd.setCursor(0,0) ;
-   lcd.print("Iniciando") ;    
-  
+  lcd.begin(16, 2);
+  lcd.setCursor(0,0) ;
+  lcd.print("Iniciando") ;    
+
   //Inicializa Servo 
   servo.attach(4) ;
-  
-  
-  
+
+
+
   // Iniciando a conexao ethernet e recebendo o IP dinamicamente
-  if (Ethernet.begin(mac) == 0) {
+  while (Ethernet.begin(mac) == 0) {
     Serial.println("Erro na rede");
-    Serial.println("Retry apos 5 segs");
+    Serial.println("Retry em 5 segs");
     // Problemas na conexao ethernet, verifique a sua rede
     delay(5000);
-    setup();
+ 
   } 
-  else {
-    ipArduino = Ethernet.localIP() ;
-    Serial.print("IP obtido por DHCP:") ;
-    Serial.println(ipArduino) ;
-    lcd.setCursor(0,0) ;
-    lcd.print("IP por DHCP") ;    
-    lcd.setCursor(0,1) ;
-    lcd.print(ipArduino) ;    
-  }  
+
+  ipArduino = Ethernet.localIP() ;
+  Serial.print("IP obtido por DHCP:") ;
+  Serial.println(ipArduino) ;
+  lcd.setCursor(0,0) ;
+  lcd.print("IP por DHCP") ;    
+  lcd.setCursor(0,1) ;
+  lcd.print(ipArduino) ;    
+
 
 }
-
-void initEthernet() {
-
-}
-
 
 void loop()
 {
@@ -105,7 +100,7 @@ void loop()
 void getMessageFromServer() {
   //tenta conectar ao servidor
   int ret = client.connect(server, port) ;
-  
+
   //Serial.println(ret,DEC)  ;
   //verifica o retorno se nao conectou encerra
   //if( !client.connect(server, port)) {
@@ -114,23 +109,23 @@ void getMessageFromServer() {
     Serial.println(ret,DEC)  ;
     return ;
   }
-  
+
   Serial.println("Conectado...") ;
   // faz requisicao ao servidor
   client.println("GET /dado/1 HTTP/1.1");
   client.println("HOST: 192.168.25.3");
   client.println();
 
-    //Aguardando conexao
+  //Aguardando conexao
   while(!client.available()){
     delay(1);
   }  
   // limpa a string json 
   jsonString ="" ;
-  
+
   //verifica se tem caracter de resposta
   int startJson = false ;
-  
+
   while( client.available()) {
     char c = client.read() ;
     if(startJson == true) {
@@ -138,26 +133,27 @@ void getMessageFromServer() {
       if(c == '}' ) {
         startJson = false ;
       }  
-        
-    } else {
+
+    } 
+    else {
       if(c == '{' ) {
         startJson = true ;
         jsonString += c ;
       }  
-      
+
     }
 
   }
-  
+
   //Serial.println(jsonString) ;
   char jsonChar[jsonString.length()];
   jsonString.toCharArray(jsonChar, jsonString.length() + 1);
   parseJson(jsonChar);  
-  
+
   while(client.connected()){
     delay(1);
   }
-  
+
   // encerra o cliente se encerrou a conexao
   if(!client.connected()) {
     client.stop() ;
@@ -187,7 +183,7 @@ void parseJson(char *jsonString)
 {
   //Inicializa o objeto Pai
   //Serial.println("Iniciando parsing ...")  ;
-  
+
   aJsonObject* root = aJson.parse(jsonString);
 
   if (root != NULL) {
@@ -213,6 +209,7 @@ void parseJson(char *jsonString)
   //Serial.println("Fim parsing ...")  ;
 
 }
+
 
 
 
